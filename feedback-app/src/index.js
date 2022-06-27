eb2f5e2998f3be6c124544ac20c1a0c144ab0320
AshleyPratt <Ashley.Pratt@target.com>
2022-06-27 17:54:00 -0400

    initial commit with the basics down. should've been committing as i went along but haha rip oh well?

---
+++ feedback-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+// this is not the way anymore. use down below
+// ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
