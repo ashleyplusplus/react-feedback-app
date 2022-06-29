a09a7b31878a5dcea3e045e0c7fa83c22fe2e7d7
AshleyPratt <Ashley.Pratt@target.com>
2022-06-29 13:48:07 -0400

    actually deletes item, asks if you're sure you want to delete first, filters through and deletes it

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -8,7 +8,9 @@ function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
     const deleteFeedback = (id) => {
-        console.log('App', id)
+        if(window.confirm('Are you sure you want to delete this item?')){
+        setFeedback(feedback.filter((item) => item.id !== id))
+        }
     }
     return (
         <>
