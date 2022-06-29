78ad86452f92a2a5395813e3aa35af53585b7012
AshleyPratt <Ashley.Pratt@target.com>
2022-06-29 13:08:31 -0400

    look into .map function

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
 export default function FeedbackList({ feedback }) {
@@ -13,4 +14,14 @@ export default function FeedbackList({ feedback }) {
         ))}
         </div>
   )
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired
+        })
+    )
 }
